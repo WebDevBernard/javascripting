b585ec97c0b9dd34048b50ee8c4f7271a55fe404
Bernard Yang <ybernard@gmail.com>
2021-05-13 01:22:07 +0000

    packing-list 2nd commit

--- lighthouse-js-fundamentals/packing-list.js
+++ lighthouse-js-fundamentals/packing-list.js
@@ -1,3 +1,4 @@
+/*
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
@@ -5,4 +6,11 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 packingList[3];
+*/
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
 
