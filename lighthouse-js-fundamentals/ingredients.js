d312c5c131f6aca274aa047d4c90c34d38a7f423
Bernard Yang <ybernard@gmail.com>
2021-05-13 01:50:09 +0000

    ingredients done

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,14 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0
+while (ingredients[i]){
+  console.log(ingredients[i])
+  i++
+}
+for (let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+let j =7
+while (ingredients[j]){
+  console.log(ingredients[j]);
+  j--
+}
